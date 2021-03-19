b57525bd798591ad5d2a96b7cdd756ad483116b6
iskrateneva <iskra.teneva3@gmail.com>
2021-03-19 19:33:23 +0200

    Added JournalModel

---
+++ Models/JournalModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Nutrition.Models
+{
+    public class JournalModel
+    {
+
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Thoughts { get; set; }
+
+        public string Gratitudes { get; set; }
+
+        public string Concerns { get; set; }
+
+        public string Questions { get; set; }
+
+        public JournalModel()
+        {
+        
+        }
+    }
+}
